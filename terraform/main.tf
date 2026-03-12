788a4ee35dcc9f6b827199148e944252fdbb6227
kusumaningrat <kusumanetcom@gmail.com>
2026-03-12 22:26:13 +0800

    Test terraform

---
+++ terraform/main.tf
@@ -0,0 +1,10 @@
+provider "nomad" {
+  address   = var.nomad_addr
+  secret_id = var.nomad_token
+}
+
+resource "nomad_job" "emoney_account" {
+  jobspec = templatefile("${path.module}/../nomad/emoney-account.nomad.hcl", {
+    IMAGE_TAG = var.image_tag
+  })
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,3 @@
+variable "nomad_addr" {}
+variable "nomad_token" {}
+variable "image_tag" {}
